35daed4a35bd86752fb732d0b213f32d20386575
Kevin Mattison and Matt Rosanio <mattisonii@yahoo.com>
2016-01-20 11:45:12 -0800

    Add jquery to register when nothing is entered

--- js/scripts.js
+++ js/scripts.js
@@ -16,7 +16,9 @@ $(function() {
   $("form#palindrome").submit(function(event) {
     var userInput = $("input#palindromeInput").val();
     var palindromeInput = palindrome(userInput);
-    if (palindromeInput === true) {
+    if (userInput.length === 0) {
+      alert("Please enter a word.");
+    } else if (palindromeInput === true) {
       $("#result").append("<p>It IS a palindrome</p>");
     } else {
       $("#result").append("<p>It is NOT a palindrome</p>");
